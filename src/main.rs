6aa02a4e2fa9f283bfc9845ca6868b85eba755a7
Brandon Pitman <bran@bran.land>
2022-07-03 20:12:33 -0700

    Reimplement as async/await.
    This isn't strictly necessary, but reqwest's blocking interface was
    already wrapping an async interface internally, so I might as well.
    
    This commit doesn't asynchronize the filesystem operations, for two
    reasons:
     * It's not immediately clear how to interop serde's deserialization
       with tokio's AsyncRead.
     * AFAICT there is no async equivalent to NamedTempFile.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,15 @@
-extern crate clap;
-#[macro_use]
-extern crate log;
-extern crate reqwest;
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_yaml;
-extern crate simple_logger;
-extern crate tempfile;
-
-use reqwest::header::USER_AGENT;
-use reqwest::StatusCode;
-use std::error;
-use std::fs::File;
-use std::io;
-use std::net::Ipv4Addr;
-use std::path::Path;
-use std::thread;
-use std::time::{Duration, Instant};
+use anyhow::{anyhow, Result};
+use log::{error, info};
+use reqwest::{
+    header::{HeaderMap, HeaderValue, USER_AGENT},
+    StatusCode,
+};
+use serde_derive::{Deserialize, Serialize};
+use std::{fs::File, io, net::Ipv4Addr, path::Path, time::Duration};
 use tempfile::NamedTempFile;
+use tokio::time::{self, MissedTickBehavior};
+
+// TODO: asynchronize filesystem operations (including NamedTempFile).
 
 #[derive(Deserialize)]
 struct Config {
@@ -32,11 +23,12 @@ struct State {
     addr: Option<Ipv4Addr>,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // Initialize, parse & verify flags.
-    simple_logger::init_with_level(log::Level::Info).expect("Could not initialize logging");
+    simple_logger::init_with_level(log::Level::Info).expect("Couldn't initialize logging");
     let flags = clap::App::new("rgdcd")
-        .version("0.1")
+        .version(env!("CARGO_PKG_VERSION"))
         .author("Brandon Pitman <bran@bran.land>")
         .about("Google Domains Dynamic DNS Client Daemon")
         .arg(
@@ -58,37 +50,44 @@ fn main() {
         .get_matches();
 
     // Parse config & state files.
+    // XXX: asynchronize?
     let state_path = Path::new(flags.value_of_os("state").unwrap());
     let cfg: Config = {
         let config_filename = flags.value_of_os("config").unwrap();
-        let config_file = File::open(config_filename).expect("Could not open config file");
-        serde_yaml::from_reader(config_file).expect("Could not parse config file")
+        let config_file = File::open(config_filename).expect("Couldn't open config file");
+        serde_yaml::from_reader(config_file).expect("Couldn't parse config file")
     };
     let mut state: State = match File::open(state_path) {
-        Ok(state_file) => serde_yaml::from_reader(state_file).expect("Could not parse state file"),
+        Ok(state_file) => serde_yaml::from_reader(state_file).expect("Couldn't parse state file"),
         Err(ref err) if err.kind() == io::ErrorKind::NotFound => State { addr: None },
-        Err(err) => panic!("Could not read state file: {}", err),
+        Err(err) => panic!("Couldn't read state file: {}", err),
     };
+
+    // Build an HTTP client.
+    let mut default_headers = HeaderMap::new();
+    default_headers.insert(
+        USER_AGENT,
+        HeaderValue::from_str(&format!("rgdcd {}", env!("CARGO_PKG_VERSION")))
+            .expect("Couldn't create default HTTP headers"),
+    );
+    let client = reqwest::Client::builder()
+        .default_headers(default_headers)
+        .build()
+        .expect("Couldn't create HTTP client");
 
     // Main loop: check IP every now and then, update if necessary.
     info!("Starting: will check & update IP every 60s");
-    let client = reqwest::blocking::Client::new();
-    let mut last_check_time = Instant::now() - Duration::from_secs(60);
+    let mut interval = time::interval(Duration::from_secs(60));
+    interval.set_missed_tick_behavior(MissedTickBehavior::Delay);
     let mut goog_addr = state.addr; // goog_addr stores our conception of what Google thinks our IP is.
     loop {
-        // Wait until the next check.
-        let next_check_time = last_check_time + Duration::from_secs(60);
-        let now = Instant::now();
-        if now < next_check_time {
-            thread::sleep(next_check_time - now);
-        }
-        last_check_time = next_check_time;
+        interval.tick().await;
 
         // Figure out what our current IP is.
-        let current_addr = match current_address(&client) {
+        let current_addr = match current_address(&client).await {
             Ok(addr) => addr,
             Err(e) => {
-                error!("Could not get current IP address: {}", e);
+                error!("Couldn't get current IP address: {}", e);
                 continue;
             }
         };
@@ -100,8 +99,8 @@ fn main() {
                 fmt_optional_addr(&goog_addr),
                 current_addr
             );
-            if let Err(err) = update_address(&cfg, &client, current_addr) {
-                error!("Could not update IP address: {}", err);
+            if let Err(err) = update_address(&cfg, &client, current_addr).await {
+                error!("Couldn't update IP address: {}", err);
                 continue;
             }
             goog_addr = Some(current_addr);
@@ -111,8 +110,8 @@ fn main() {
         if Some(current_addr) != state.addr {
             let mut new_state = state.clone();
             new_state.addr = Some(current_addr);
-            if let Err(err) = update_state(state_path, &new_state) {
-                error!("Could not write state file: {}", err);
+            if let Err(err) = update_state(state_path, &new_state).await {
+                error!("Couldn't write state file: {}", err);
                 continue;
             }
             state = new_state;
@@ -127,41 +126,37 @@ fn fmt_optional_addr(addr: &Option<Ipv4Addr>) -> String {
     }
 }
 
-fn current_address(client: &reqwest::blocking::Client) -> Result<Ipv4Addr, Box<dyn error::Error>> {
+async fn current_address(client: &reqwest::Client) -> Result<Ipv4Addr> {
     let resp = client
         .get("https://domains.google.com/checkip")
-        .header(USER_AGENT, "rgdcd 1.0")
-        .send()?;
+        .send()
+        .await?;
     if resp.status() != StatusCode::OK {
-        return Err(format!("unexpected status code: {}", resp.status()).into());
+        return Err(anyhow!("unexpected status code: {}", resp.status()));
     }
-    Ok(resp.text()?.parse()?)
+    Ok(resp.text().await?.parse()?)
 }
 
-fn update_address(
-    cfg: &Config,
-    client: &reqwest::blocking::Client,
-    addr: Ipv4Addr,
-) -> Result<(), Box<dyn error::Error>> {
+async fn update_address(cfg: &Config, client: &reqwest::Client, addr: Ipv4Addr) -> Result<()> {
     let resp = client
         .post("https://domains.google.com/nic/update")
-        .header(USER_AGENT, "rgdcd 1.0")
         .basic_auth(&cfg.username, Some(&cfg.password))
         .query(&[("hostname", &cfg.hostname), ("myip", &addr.to_string())])
         .body("")
-        .send()?;
+        .send()
+        .await?;
     let status = resp.status();
-    let txt = resp.text()?;
+    let txt = resp.text().await?;
     if txt.starts_with("good") {
         return Ok(());
     }
-    Err(format!("update request got error: {} ({})", txt, status).into())
+    Err(anyhow!("update request got error: {} ({})", txt, status))
 }
 
-fn update_state(state_path: &Path, state: &State) -> Result<(), Box<dyn error::Error>> {
+async fn update_state(state_path: &Path, state: &State) -> Result<()> {
     let dir = state_path.parent().ok_or_else(|| {
-        format!(
-            "could not determine parent directory of {}",
+        anyhow!(
+            "couldn't determine parent directory of {}",
             state_path.display()
         )
     })?;
